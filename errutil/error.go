5bb8a4dd1ff4ef47c6ebd07d9633f551c91528cb
haolei <haol@coinv.com>
2019-01-23 14:27:43 +0800

    add errutil

---
+++ errutil/error.go
@@ -0,0 +1,23 @@
+package errutil
+
+//ErrResult return err state
+// defStr[0], defStr[1] = fail, succeed comment
+func ErrResult(err error, defStr ...string) string {
+	var (
+		defFail    = "fail"
+		defSucceed = "succeed"
+	)
+
+	if len(defStr) > 0 {
+		defFail = defStr[0]
+	}
+	if len(defStr) > 1 {
+		defSucceed = defStr[1]
+	}
+
+	if err != nil {
+		return defFail + ":" + err.Error()
+	}
+
+	return defSucceed
+}
