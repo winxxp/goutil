d82b2ef6a5ff4cfded939abcfa775c3df613ffe3
winxxp <haolei2008@gmail.com>
2019-05-09 17:43:35 +0800

    Add HexByte

---
+++ byteutil/hexbyte/hexbyte.go
@@ -0,0 +1,20 @@
+package hexbyte
+
+import "fmt"
+
+type HexByte byte
+
+func (b HexByte) String() string {
+	return fmt.Sprintf("%02X", byte(b))
+}
+
+type HexBytes []byte
+
+func (b HexBytes) String() string {
+	var hb = make([]HexByte, len(b))
+	for i, v := range b {
+		hb[i] = HexByte(v)
+	}
+
+	return fmt.Sprint(hb)
+}
