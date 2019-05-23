b94b7bad9a12d807cd407264bda48afffb7f9113
winxxp <haolei2008@gmail.com>
2019-05-23 14:54:10 +0800

    matcher: add http matcher

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ require (
 	github.com/bwmarrin/snowflake v0.0.0-20180412010544-68117e6bbede
 	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
 	github.com/gin-gonic/gin v1.3.0
-	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20190328170749-bb2674552d8f // indirect
 	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/juju/errors v0.0.0-20181118221551-089d3ea4e4d5
@@ -14,16 +14,18 @@ require (
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/smartystreets/assertions v0.0.0-20190401211740-f487f9de1cd3 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a
 	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/ugorji/go/codec v0.0.0-20181209151446-772ced7fd4c2 // indirect
 	github.com/winxxp/glog v1.0.6
-	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c // indirect
-	golang.org/x/net v0.0.0-20190328230028-74de082e2cca // indirect
-	golang.org/x/sys v0.0.0-20190402142545-baf5eb976a8c // indirect
-	golang.org/x/text v0.3.0
-	golang.org/x/tools v0.0.0-20190402200628-202502a5a924 // indirect
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
+	golang.org/x/net v0.0.0-20190514140710-3ec191127204 // indirect
+	golang.org/x/sys v0.0.0-20190516110030-61b9204099cb // indirect
+	golang.org/x/text v0.3.2
+	golang.org/x/tools v0.0.0-20190517183331-d88f79806bbd // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
---
+++ testutil/matcher/http.go
@@ -0,0 +1,134 @@
+package matcher
+
+import (
+	"errors"
+	"fmt"
+	"github.com/onsi/gomega"
+	"github.com/onsi/gomega/format"
+	"net/http"
+	"reflect"
+)
+
+func ShouldHttpOK(actual interface{}, expected ...interface{}) string {
+	code, ok := actual.(int)
+	if !ok {
+		return fmt.Sprintf("actual not RespondRecorder type %v", reflect.TypeOf(actual))
+	}
+
+	expectedCode := http.StatusOK
+	if code == expectedCode {
+		return ""
+	}
+
+	return fmt.Sprintf("http code: %d not %d", code, expectedCode)
+}
+
+func ShouldHttpCreated(actual interface{}, expected ...interface{}) string {
+	code, ok := actual.(int)
+	if !ok {
+		return fmt.Sprintf("actual not RespondRecorder type %v", reflect.TypeOf(actual))
+	}
+
+	expectedCode := http.StatusCreated
+	if code == expectedCode {
+		return ""
+	}
+
+	return fmt.Sprintf("http code: %d not %d", code, expectedCode)
+}
+
+func ShouldHttpNoContent(actual interface{}, expected ...interface{}) string {
+	code, ok := actual.(int)
+	if !ok {
+		return fmt.Sprintf("actual not RespondRecorder type %v", reflect.TypeOf(actual))
+	}
+
+	expectedCode := http.StatusNoContent
+	if code == expectedCode {
+		return ""
+	}
+
+	return fmt.Sprintf("http code: %d not %d", code, expectedCode)
+}
+
+func ShouldHttpBadRequest(actual interface{}, expected ...interface{}) string {
+	code, ok := actual.(int)
+	if !ok {
+		return fmt.Sprintf("actual not RespondRecorder type %v", reflect.TypeOf(actual))
+	}
+
+	expectedCode := http.StatusBadRequest
+	if code == expectedCode {
+		return ""
+	}
+
+	return fmt.Sprintf("http code: %d not %d", code, expectedCode)
+}
+
+func ShouldHttpNotFound(actual interface{}, expected ...interface{}) string {
+	code, ok := actual.(int)
+	if !ok {
+		return fmt.Sprintf("actual not RespondRecorder type %v", reflect.TypeOf(actual))
+	}
+
+	expectedCode := http.StatusNotFound
+	if code == expectedCode {
+		return ""
+	}
+
+	return fmt.Sprintf("http code: %d not %d", code, expectedCode)
+}
+
+func ShouldHttpInternalServerError(actual interface{}, expected ...interface{}) string {
+	code, ok := actual.(int)
+	if !ok {
+		return fmt.Sprintf("actual not RespondRecorder type %v", reflect.TypeOf(actual))
+	}
+
+	expectedCode := http.StatusInternalServerError
+	if code == expectedCode {
+		return ""
+	}
+
+	return fmt.Sprintf("http code: %d not %d", code, expectedCode)
+}
+
+type beHttpCode struct {
+	code int
+}
+
+func BeHttpStatusOK() gomega.OmegaMatcher {
+	return &beHttpCode{code: http.StatusOK}
+}
+
+func BeHttpCreated() gomega.OmegaMatcher {
+	return &beHttpCode{code: http.StatusCreated}
+}
+func BeHttpNoContent() gomega.OmegaMatcher {
+	return &beHttpCode{code: http.StatusNoContent}
+}
+func BeHttpBadRequest() gomega.OmegaMatcher {
+	return &beHttpCode{code: http.StatusBadRequest}
+}
+func BeHttpNotFound() gomega.OmegaMatcher {
+	return &beHttpCode{code: http.StatusNotFound}
+}
+func BeHttpInternalServerError() gomega.OmegaMatcher {
+	return &beHttpCode{code: http.StatusInternalServerError}
+}
+
+func (b *beHttpCode) Match(actual interface{}) (success bool, err error) {
+	if code, ok := actual.(int); ok {
+		return b.code == code, nil
+	}
+	err = errors.New(format.Message(actual, "HTTP Code", b.code))
+	return
+}
+
+func (b *beHttpCode) FailureMessage(actual interface{}) (message string) {
+	return format.Message(actual, "bo be http code", b.code)
+}
+
+func (b *beHttpCode) NegatedFailureMessage(actual interface{}) (message string) {
+	return format.Message(actual, "not bo be http code", b.code)
+}
